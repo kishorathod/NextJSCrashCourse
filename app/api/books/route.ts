a1a64785d090189d2b07f6ffeb451b5e855d5937
kishorathod <krr8088@gmail.com>
2025-11-14 00:49:33 +0530

    Added backend code exporser with simple CRUD operation

---
+++ app/api/books/route.ts
@@ -0,0 +1,14 @@
+// app/ api/ books/ route.ts
+
+import books from '@/app/api/db';
+
+export async function GET(){
+    return Response.json(books);
+}
+
+export async function POST(request: Request){
+    const book = await request.json();
+    books.push(book);
+
+    return Response.json(books);
+}
---
+++ app/api/db.ts
@@ -0,0 +1,9 @@
+// app/api/db.ts
+
+const books = [
+    {id:1, name:"Atomic Habbits"},
+    {id:2, name:"Deep Work"},
+    {id:3, name:"The 7 Habbits of Effective Poeple"},
+];
+
+export  default  books;
