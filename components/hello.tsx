85d906b596f7da29e305e4a98959389098b11858
kishorathod <krr8088@gmail.com>
2025-11-13 21:10:19 +0530

    first commit

---
+++ components/hello.tsx
@@ -0,0 +1,10 @@
+'use client';     // a cleint directive which renders this component on the client side
+
+const Hello = () => {
+    console.log('Hello World!');
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Hello
